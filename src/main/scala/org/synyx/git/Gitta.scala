26d8382e258c5277b1740d58ee1247e54be40f98
Florian Hopf <fhopf@web.de>
2011-08-23 23:37:30 +0200

    messages help and exit work

--- src/main/scala/org/synyx/git/Gitta.scala
+++ src/main/scala/org/synyx/git/Gitta.scala
@@ -1,20 +1,35 @@
 package org.synyx.git
 
 import org.jibble.pircbot.PircBot
+import math.Ordering.String
 
 class Gitta extends PircBot {
 
   setName("Gitta")
 
   override def onMessage(channel: String, sender: String, login: String, hostname: String, message: String) = {
-    if (message.trim().startsWith("help")) {
-      sendMessage(channel, helpMessage());
-    } else if (message.trim().equals("exit")) {
+    val priv = privateMessage(message)
+    if (priv.equals("help")) {
+      sendHelpMessage(channel)
+    } else if (priv.equals("exit")) {
       System.exit(0)
     }
   }
 
-  def helpMessage() = {
-    "Commands I understand: help (prints this message)\\n exit"
+  def sendHelpMessage(channel: String) = {
+    sendMessage(channel, "Commands I understand: help (prints this message)")
+    sendMessage(channel, "exit")
+  }
+
+  def privateMessage(message: String) = {
+    if (message.startsWith(getName())) {
+      // name + :
+      val priv = message.substring(getName().length() + 1, message.length()).trim()
+      println("got message: " + priv)
+      priv
+    } else {
+      ""
+    }
+
   }
 }
---
+++ src/test/scala/org/synyx/git/GittaTest.scala
@@ -0,0 +1,20 @@
+package org.synyx.git
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+
+@RunWith(classOf[JUnitRunner])
+class GittaTest extends FunSuite with ShouldMatchers {
+
+  test("message help") {
+    val gitta = new Gitta()
+    gitta.privateMessage("Gitta: help") should be("help")
+  }
+
+  test("ignored message") {
+    val gitta = new Gitta()
+    gitta.privateMessage("ignored message") should be("")
+  }
+}
